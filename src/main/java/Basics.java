ba466e176d848f81f0d893c644bbe296883e894e
Joshua Cuevas <joshua.cuevas@mail.utoronto.ca>
2022-09-25 21:50:44 -0400

    finished

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i>=0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,12 +146,17 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] split_spaces = to_split.split(" ");
+        for (String a : split_spaces){
+            ret.append(a.charAt(0));
+        }
+
+
 
         // Fill in the rest of the body here
 
         return ret.toString();
     }
-
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
      *    of integers). The method's name is 'oddSum' and it returns an int
@@ -171,6 +181,13 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        if (arr.length < 1){
+            return current_sum;
+        }
+        for (int i = 1; i < arr.length; i += 2){
+            current_sum += arr[i];
+
+        }
         return current_sum;
     }
 
